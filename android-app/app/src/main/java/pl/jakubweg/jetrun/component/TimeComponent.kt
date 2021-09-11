b885f39a64b16ea423be569e6f605864c5293152
jakweg <jakubek.weg@gmail.com>
2021-09-11 20:18:07 +0200

    Add time component for obtaining system time

---
+++ android-app/app/src/main/java/pl/jakubweg/jetrun/component/TimeComponent.kt
@@ -0,0 +1,29 @@
+package pl.jakubweg.jetrun.component
+
+interface TimeComponent {
+    fun currentTimeMillis(): Long
+}
+
+class RealTimeComponent : TimeComponent {
+    override fun currentTimeMillis(): Long {
+        return System.currentTimeMillis()
+    }
+}
+
+class FakeTimeComponent : TimeComponent {
+    private var _currentTime = 0L
+
+    override fun currentTimeMillis(): Long {
+        return _currentTime
+    }
+
+    fun advanceTimeMillis(millis: Long) {
+        require(millis >= 0) { "Millis are required to be >= 0, got $millis" }
+        this.setTimeMillis(this._currentTime + millis)
+    }
+
+    fun setTimeMillis(millis: Long) {
+        require(millis >= 0) { "Millis are required to be >= 0, got $millis" }
+        this._currentTime = millis
+    }
+}
